6f7f550e1cd6b7de9b7771a0026dc7aa1506ed45
DavidMaya944 <maya.quiros.david.jesus@gmail.com>
2021-01-04 02:46:31 +0100

    Modificación en la vista de Cliente.

--- DI-Cliente/src/view/FrmRegistro.java
+++ DI-Cliente/src/view/FrmRegistro.java
@@ -10,9 +10,12 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
+
 import controller.Control;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class FrmRegistro extends JFrame {
 	
