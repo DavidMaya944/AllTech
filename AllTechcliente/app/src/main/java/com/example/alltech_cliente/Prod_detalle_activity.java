b297f833667c2d234d73be1735a5c6e9124d2f22
DavidMaya944 <maya.quiros.david.jesus@gmail.com>
2021-01-22 11:49:36 +0100

    Se ha creado los click listener de los botones de prod detalle.

--- AllTechcliente/app/src/main/java/com/example/alltech_cliente/Prod_detalle_activity.java
+++ AllTechcliente/app/src/main/java/com/example/alltech_cliente/Prod_detalle_activity.java
@@ -3,12 +3,38 @@ package com.example.alltech_cliente;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Prod_detalle_activity extends AppCompatActivity {
 
+    EditText txtNombre;
+    EditText txtPrecio;
+    EditText txtDescripcion;
+    Button btnAdd;
+    Button btnVolver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.prod_detalle_activity);
+        
+        btnAdd = findViewById(R.id.btnAdd);
+        btnVolver = findViewById(R.id.btnVolver);
+        
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Añadido a la cesta", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
