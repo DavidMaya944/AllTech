15ac9d42a476dece0bb19df4b7fe10b431cbc542
DavidMaya944 <maya.quiros.david.jesus@gmail.com>
2021-02-02 20:29:54 +0100

    Se ha creado la activity ajustes de usuario.

---
+++ AllTechcliente/app/src/main/java/com/example/alltech_cliente/ActivityAjustes.java
@@ -0,0 +1,14 @@
+package com.example.alltech_cliente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityAjustes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajustes);
+    }
+}
